3bd6ccc669289a501b7016a9b2ab1daddfeb8114
Hamthan <muhammadhamthan660@gmail.com>
2025-09-13 14:14:36 +0530

    check github action workflow with ec2 instnaces

--- flaskapp.py
+++ flaskapp.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/home")
 def home():
-    return "Welcome to home page"
+    return "Welcome to home page Let's begin"
 #GITHUB ACTION CHECK HOW IT WORK
 #new one to tooo
 
